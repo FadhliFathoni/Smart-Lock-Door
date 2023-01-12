78a4244ced0faf646426ca8115e4af77b20f2c9d
FadhliFathoni <fadhlifathoni62@gmail.com>
2023-01-12 08:53:56 +0700

    Add Requirement

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, Group
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from .forms import LoginForm
 
 def loginView(request):
@@ -8,4 +8,9 @@ def loginView(request):
     context = {
         "form":form
     }
+    uname = request.POST['username']
+    pwd = request.POST['password']
+    akun = authenticate(user = uname, password = pwd)
+    if akun is not None:
+        login(request, akun)
     return render(request,'login.html',context)
